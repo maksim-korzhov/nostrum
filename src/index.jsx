460578ae7530d446c9df15d957b3a760c29124cb
Maksim Korzhov <korzhov.m.s@yandex.ru>
2017-11-11 16:40:21 +0000

    ✨ Added redux-saga.

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,32 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import {createStore, applyMiddleware} from "redux";
-import {BrowserRouter, Route, Switch, IndexRoute} from "react-router-dom";
-import {composeWithDevTools} from "redux-devtools-extension";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route, Switch, IndexRoute } from "react-router-dom";
+import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from "redux-saga";
 
 import reducers from "./reducers";
+import sagas from "./sagas";
 
 import Main from "./containers/Main";
 
 import style from "../style/style.scss";
 
-const createStoreWithMiddleware = composeWithDevTools(applyMiddleware())(createStore);
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware();
 
+// compose saga with store
+const createStoreWithMiddleware = composeWithDevTools(applyMiddleware(sagaMiddleware))(createStore);
+
+const store = createStoreWithMiddleware(reducers);
+
+// run the saga
+sagaMiddleware.run(sagas);
+
+// render the application
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
+    <Provider store={store}>
         <BrowserRouter>
             {/*<Wrapper>*/}
                 <Switch>
